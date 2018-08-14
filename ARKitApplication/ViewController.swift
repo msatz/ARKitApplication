9459d60d095b11d0e772bfdf87a4ee44cf9dfd9f
smac <smac@smacs-mac-mini.local>
2018-08-14 11:45:39 +0530

    Initial Commit

---
+++ ARKitApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitApplication
+//
+//  Created by smac on 8/14/18.
+//  Copyright © 2018 smac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
